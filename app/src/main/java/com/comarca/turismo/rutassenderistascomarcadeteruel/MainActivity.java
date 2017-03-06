accad02d02ce193dc9e4e32f9c8cedc4f3c2f325
drekker <drekker@gmail.com>
2017-03-06 21:06:02 +0100

    Initial commit de verdad

---
+++ app/src/main/java/com/comarca/turismo/rutassenderistascomarcadeteruel/MainActivity.java
@@ -0,0 +1,20 @@
+package com.comarca.turismo.rutassenderistascomarcadeteruel;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void lanzaAndatela(View view){
+        Intent intent = new Intent (this, RutasAndatela.class);
+        startActivity(intent);
+    }
+}
+
